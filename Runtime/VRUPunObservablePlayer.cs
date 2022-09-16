11af7cd05fc65555eed6b6a44ccf4b3e080ff684
Ramon <ramon.oliva.martinez@gmail.com>
2022-09-16 19:48:31 +0200

    *

--- Runtime/VRUPunObservablePlayer.cs
+++ Runtime/VRUPunObservablePlayer.cs
@@ -9,38 +9,44 @@ namespace VRUnited
     public class VRUPunObservablePlayer : VRUPunObservableBase
     {
 
-        public delegate void VRUPunObservablePlayerAction(VRUPunObservablePlayer player);
-        public static event VRUPunObservablePlayerAction OnVRUPlayerEnabled;
-        public static event VRUPunObservablePlayerAction OnVRUPlayerDisabled;
+        //public delegate void VRUPunObservablePlayerAction(VRUPunObservablePlayer player);
+        //public static event VRUPunObservablePlayerAction OnVRUPlayerEnabled;
+        //public static event VRUPunObservablePlayerAction OnVRUPlayerDisabled;
+
+        public static HashSet<VRUPunObservablePlayer> _instances = new HashSet<VRUPunObservablePlayer>();
 
         public override void OnEnable()
         {
             base.OnEnable();
 
-            if (OnVRUPlayerEnabled != null)
-            {
-                OnVRUPlayerEnabled(this);
-            }
+            //if (OnVRUPlayerEnabled != null)
+            //{
+            //    OnVRUPlayerEnabled(this);
+            //}
+
+            _instances.Add(this);
         }
 
         protected override void Start()
         {
             base.Start();
 
-            if (OnVRUPlayerEnabled != null)
-            {
-                OnVRUPlayerEnabled(this);
-            }
+            //if (OnVRUPlayerEnabled != null)
+            //{
+            //    OnVRUPlayerEnabled(this);
+            //}
         }
 
         public override void OnDisable()
         {
             base.OnDisable();
 
-            if (OnVRUPlayerDisabled != null)
-            {
-                OnVRUPlayerDisabled(this);
-            }
+            _instances.Remove(this);
+
+            //if (OnVRUPlayerDisabled != null)
+            //{
+            //    OnVRUPlayerDisabled(this);
+            //}
         }
 
     }
