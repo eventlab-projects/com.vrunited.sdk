7f1fc2cfa8be73c6d9b6dbb36a89353fd06cd494
Ramon <ramon.oliva.martinez@gmail.com>
2022-09-16 16:37:33 +0200

    * Added VRUPunObservablePlayer

---
+++ Runtime/VRUPunObservablePlayer.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRUnited
+{
+
+    public class VRUPunObservablePlayer : VRUPunObservableBase
+    {
+
+        public delegate void VRUPunObservablePlayerAction(VRUPunObservablePlayer player);
+        public static event VRUPunObservablePlayerAction OnVRUPlayerEnabled;
+        public static event VRUPunObservablePlayerAction OnVRUPlayerDisabled;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            if (OnVRUPlayerEnabled != null)
+            {
+                OnVRUPlayerEnabled(this);
+            }
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+
+            if (OnVRUPlayerDisabled != null)
+            {
+                OnVRUPlayerDisabled(this);
+            }
+        }
+
+    }
+
+}
+
+
